42d779133f7d2edcc11375e0198f68d8cd768201
sefirant <sefirarianti52@gmail.com>
2018-10-27 12:55:33 +0700

    edit delete

--- index.php
+++ index.php
@@ -34,7 +34,11 @@
 			<td><?php echo $data['password']; ?></td>
 			<td><?php echo $data['level']; ?></td>
 			<td><?php echo $data['fullname']; ?></td>
-			
+			<td>
+			<a onclick="return confirm('Yakin untuk menghapus ?')"
+			href="delete.php?id=<?php echo $data['id']; ?>">Hapus</a>
+			<a href="edit.php?id=<?php echo $data['id']; ?>">Edit</a>
+			</td>
 
 		</tr>
 
